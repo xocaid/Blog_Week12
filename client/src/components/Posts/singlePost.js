e3070aae57b44f4d85d640226818f4a438bf5c54
xocaid <xocaid@gmail.com>
2022-12-05 10:46:11 -0800

    Adding db sql

--- client/src/components/Posts/singlePost.js
+++ client/src/components/Posts/singlePost.js
@@ -35,13 +35,13 @@ const SinglePost = ({ singlePost }) => {
           <div className="card_body">
             <p>{singlePost.post.substring(0, 40)}...</p>
             {/* <p>{singlePost.post}</p> */}
-            <button className='singlePost-btn' onClick={handleFlipPost}>View More</button>
+            <button className='singlePost-btn' onClick={handleFlipPost}>Read More</button>
           </div>
         </div>
       ) : (
         <div>
           <div className="card_image-full">
-            <BlogImage img={singlePost.image} alt={singlePost.id} />
+            <BlogImage img={singlePost.image ? singlePost.image : 'https://cdn.elearningindustry.com/wp-content/uploads/2016/05/top-10-books-every-college-student-read-1024x640.jpeg'} alt={singlePost.id} />
           </div>
           <div className="card_body">
             <p>{singlePost.post}</p>
